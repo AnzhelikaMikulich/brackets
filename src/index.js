b17c744cf19a245d35ba35b0418ad37baba0f55b
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-07-19 07:02:51 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+      return false;
+  }
+  str = str.split("");
+  console.log(str);
+  let brackets = {};
+  let arr = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  console.log(brackets);
+  for (let j = 0; j < str.length; j++) {
+      if (brackets[arr[arr.length - 1]] === str[j]) {
+          arr.pop();
+      } else {
+          arr.push(str[j]);
+      }
+      console.log(arr);
+  }
+
+  return arr.length === 0;
+};
